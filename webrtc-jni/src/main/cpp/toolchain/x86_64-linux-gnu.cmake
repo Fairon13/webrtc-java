871b2787f7d5e9dff5794decbaadb7eb0268b19e
Федосеев Антон Ильич <fedoseevai@sibur.ru>
2024-06-06 17:31:33 +0300

    Build setup

--- webrtc-jni/src/main/cpp/toolchain/x86_64-linux-gnu.cmake
+++ webrtc-jni/src/main/cpp/toolchain/x86_64-linux-gnu.cmake
@@ -5,3 +5,12 @@ set(TARGET_CPU				"x64")
 
 set(SYSROOT_ARCH			"amd64")
 set(SYSROOT_PATH			"src/build/linux/debian_sid_amd64-sysroot")
+
+set(CMAKE_SYSROOT /home/fairon/work/smart-chemodan/streamer/build-webrtc-java/src/build/linux/debian_sid_amd64-sysroot)
+set(tools         /home/fairon/work/smart-chemodan/streamer/build-webrtc-java/src/third_party/llvm-build/Release+Asserts)
+
+set(CMAKE_C_COMPILER ${tools}/bin/clang)
+set(CMAKE_C_COMPILER_TARGET ${triple})
+set(CMAKE_CXX_COMPILER ${tools}/bin/clang++)
+set(CMAKE_CXX_COMPILER_TARGET ${triple})
+add_link_options("-fuse-ld=lld")
